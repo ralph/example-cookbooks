b6fd5eac90f274b40ab37903a48ce6790090ecdb
Jonathan Weiss <jw@innerewut.de>
2011-04-12 14:40:50 +0200

    make NFS attributes work without NFS client/server

--- nfs/attributes/default.rb
+++ nfs/attributes/default.rb
@@ -1,5 +1,5 @@
 default[:nfs][:exports] = ["/vol/data"]
-default[:nfs][:clients] = scalarium[:roles]['php-app'][:instances].values.map{|config| config[:private_ip]}
-default[:nfs][:server] = scalarium[:roles]['nfs'][:instances].values.map{|config| config[:private_ip]}.first
+default[:nfs][:clients] = scalarium[:roles]['php-app'][:instances].values.map{|config| config[:private_ip]} rescue []
+default[:nfs][:server] = scalarium[:roles]['nfs'][:instances].values.map{|config| config[:private_ip]}.first rescue nil
 
 default[:nfs][:mount_options] = %w(rsize=32768,wsize=32768,bg,hard,nfsvers=3,intr,tcp)
