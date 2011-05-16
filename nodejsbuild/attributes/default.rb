791d0797c03ef0e3066c35ae6fdc65974123d832
Daniel Hüsch <daniel.huesch@gmail.com>
2011-05-16 13:44:15 +0200

    wip

--- nodejsbuild/attributes/default.rb
+++ nodejsbuild/attributes/default.rb
@@ -4,7 +4,7 @@ default[:nodejsbuild][:basename] = "nodejs-#{node[:nodejsbuild][:version]}"
 default[:nodejsbuild][:pkgrelease] = '1'
 default[:nodejsbuild][:prefix] = '/usr/local'
 default[:nodejsbuild][:arch] = node[:kernel][:machine] == 'x86_64' ? 'amd64' : 'i386'
-default[:nodejsbuild][:deb] = "nodejs_#{node[:nodejsbuild][:version]}.#{node[:nodejsbuild][:pkgrelease]}_#{node[:nodejsbuild][:arch]}.deb"
+default[:nodejsbuild][:deb] = "nodejs_#{node[:nodejsbuild][:version]}-#{node[:nodejsbuild][:pkgrelease]}_#{node[:nodejsbuild][:arch]}.deb"
 default[:nodejsbuild][:s3] = {}
 default[:nodejsbuild][:s3][:upload] = false
 default[:nodejsbuild][:s3][:bucket] = ''
--- nodejsbuild/recipes/default.rb
+++ nodejsbuild/recipes/default.rb
@@ -2,7 +2,7 @@ execute "apt-get update"
 
 package "s3cmd" do
   only_if do
-    node[:rubybuild][:s3][:upload]
+    node[:nodejsbuild][:s3][:upload]
   end
 end
 
@@ -40,37 +40,43 @@ case node[:platform]
     package "libssl-dev"
 end
 
-remote_file "/tmp/#{node[:nodejsbuild][:basename]}.tar.gz" do
-  source "http://nodejs.org/dist/node-v#{node[:nodejsbuild][:version]}.tar.gz"
-end
+('0.4.0' .. '0.4.7').each do |version|
+  node[:nodejsbuild][:version] = version
+  node[:nodejsbuild][:basename] = "nodejs-#{node[:nodejsbuild][:version]}"
+  node[:nodejsbuild][:deb] = "nodejs_#{node[:nodejsbuild][:version]}-#{node[:nodejsbuild][:pkgrelease]}_#{node[:nodejsbuild][:arch]}.deb"
 
-execute "tar xvfz #{node[:nodejsbuild][:basename]}.tar.gz" do
-  cwd "/tmp"
-end
-
-execute "./configure --prefix=#{node[:rubybuild][:prefix]}" do
-  cwd "/tmp/node-v#{node[:nodejsbuild][:version]}"
-end
-
-execute "checkinstall -y -D --pkgname=nodejs --pkgversion=#{node[:nodejsbuild][:version]} --pkgrelease=#{node[:nodejsbuild][:pkgrelease]} --maintainer=daniel.huesch@scalarium.com --pkglicense='node.js License' make all install" do
-  cwd "/tmp/node-v#{node[:nodejsbuild][:version]}"
-end
-
-template "/tmp/.s3cfg" do
-  source "s3cfg.erb"
-  only_if do
-    node[:nodejsbuild][:s3][:upload]
+  remote_file "/tmp/#{node[:nodejsbuild][:basename]}.tar.gz" do
+    source "http://nodejs.org/dist/node-v#{node[:nodejsbuild][:version]}.tar.gz"
   end
-end
 
-execute "s3cmd -c /tmp/.s3cfg put --acl-public --guess-mime-type #{node[:nodejsbuild][:deb]} s3://#{node[:nodejsbuild][:s3][:bucket]}/#{node[:nodejsbuild][:s3][:path]}/" do
-  cwd "/tmp/node-v#{node[:nodejsbuild][:version]}"
-  only_if do
-    node[:nodejsbuild][:s3][:upload]
+  execute "tar xvfz #{node[:nodejsbuild][:basename]}.tar.gz" do
+    cwd "/tmp"
   end
-end
 
-file "/tmp/.s3cfg" do
-  action :delete
-  backup false
+  execute "./configure --prefix=#{node[:nodejsbuild][:prefix]}" do
+    cwd "/tmp/node-v#{node[:nodejsbuild][:version]}"
+  end
+
+  execute "checkinstall -y -D --pkgname=nodejs --pkgversion=#{node[:nodejsbuild][:version]} --pkgrelease=#{node[:nodejsbuild][:pkgrelease]} --maintainer=daniel.huesch@scalarium.com --pkglicense='node.js License' make all install" do
+    cwd "/tmp/node-v#{node[:nodejsbuild][:version]}"
+  end
+
+  template "/tmp/.s3cfg" do
+    source "s3cfg.erb"
+    only_if do
+      node[:nodejsbuild][:s3][:upload]
+    end
+  end
+
+  execute "s3cmd -c /tmp/.s3cfg put --acl-public --guess-mime-type #{node[:nodejsbuild][:deb]} s3://#{node[:nodejsbuild][:s3][:bucket]}/#{node[:nodejsbuild][:s3][:path]}/" do
+    cwd "/tmp/node-v#{node[:nodejsbuild][:version]}"
+    only_if do
+      node[:nodejsbuild][:s3][:upload]
+    end
+  end
+
+  file "/tmp/.s3cfg" do
+    action :delete
+    backup false
+  end
 end
